33f3f78ae1cbd01b8bde4f9292a988fc17c79d7f
David Stover <dgstover@verizon.net>
2017-03-25 16:13:53 -0700

    Added routines for zlib inflate/deflates support

---
+++ GritApp/ZlibUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+public static class ZlibUtil
+{
+    public static void Deflate(string sourceFile, string destinationFile)
+    {
+        var sourceInfo = new FileInfo(sourceFile);
+        using (var sourceFileStream = sourceInfo.OpenRead())
+        {
+            using (var destinationFileStream = File.Create(destinationFile))
+            {
+                using (var deflateStream = new DeflateStream(destinationFileStream, CompressionMode.Compress))
+                {
+                    sourceFileStream.CopyTo(deflateStream);
+                }
+            }
+        }
+    }
+
+    public static void Inflate(string sourceFile, string destinationFile)
+    {
+        var sourceInfo = new FileInfo(sourceFile);
+        using (var sourceFileStream = sourceInfo.OpenRead())
+        {
+            using (var destinationFileStream = File.Create(destinationFile))
+            {
+                using (var deflateStream = new DeflateStream(sourceFileStream, CompressionMode.Decompress))
+                {
+                    deflateStream.CopyTo(destinationFileStream);
+                }
+            }
+        }
+    }
+}
