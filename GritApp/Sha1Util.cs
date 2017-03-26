0885f2235e064c7fbb8045ab17001f0c94e41dc3
David Stover <dgstover@verizon.net>
2017-03-26 02:30:56 -0700

    Added support to compute SHA1 hash

---
+++ GritApp/Sha1Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public static class Sha1Util
+{
+    public static byte[] ComputeHash(byte[] data)
+    {
+        var provider = new SHA1CryptoServiceProvider();
+        return provider.ComputeHash(data);
+    }
+}
